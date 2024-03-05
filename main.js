509e93e8a85a7640bc690e85f227db520a0f6a86
Daecko <properlydone485@gmail.com>
2024-03-05 14:23:51 -0600

    final commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Entero ",10);
+console.log("Flotante ",1.5);
+console.log("String \"glob\"");
+console.log("Booleano ", true);
+console.log("Array",[1,2,3,4]);
+console.log("Objeto ",{name:"Dog",paws:4});
